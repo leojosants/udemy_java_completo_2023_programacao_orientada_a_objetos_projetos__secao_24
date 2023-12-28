f3a0ac6b056cf7f5302625cc84797552ce6ba098
Leonardo Santos <leonardojosantos@gmail.com>
2023-12-28 16:11:13 -0300

    Project created

---
+++ workshopmongo/src/main/java/com/example/demo/WorkshopmongoApplication.java
@@ -0,0 +1,16 @@
+/*------------------- packages -------------------*/
+package com.example.demo;
+
+/*------------------- imports -------------------*/
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*------------------- class WorkshopmongoApplication -------------------*/
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	/*------------------- main method -------------------*/
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+}
